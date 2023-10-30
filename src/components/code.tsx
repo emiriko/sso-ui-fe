1b6166fcdbc12286ecbf8bc4ebe7be6d87704738
emilicos <notalvaroaustin@gmail.com>
2023-10-31 00:32:35 +0700

    feat: adding UI for fe

---
+++ src/components/code.tsx
@@ -0,0 +1,7 @@
+export const Code: React.FC<{
+    children: React.ReactNode
+}> = ({
+    children
+}) => (
+    <code className="font-mono font-bold text-sky-500 dark:text-sky-400">{children}</code>
+)
